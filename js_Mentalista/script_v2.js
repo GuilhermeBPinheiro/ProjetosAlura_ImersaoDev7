1c4c529644a6d57045f14757840d995759ee6b49
Guilherme Bueno Pinheiro <57289531+GuilhermeBPinheiro@users.noreply.github.com>
2023-11-21 15:45:20 -0300

    Update script_v2.js

--- js_Mentalista/script_v2.js
+++ js_Mentalista/script_v2.js
@@ -1,13 +1,14 @@
 // Projeto Conversor de Moeda
 
 /*
-Versão: 1.0
-Data: 21/11/2023
+Versão: 2.0
+Data: 22/11/2023
 Descrição: Este programa vai gerar um número e você tem que digitar o número até acertar.
 Observação: Modelo de script fornecido na primeira aula de imersão no Dev 7ª edição.
 */
 
-let numeroAleatorio = Math.floor(Math.random() * 1000) + 1; // Faça o computador gerar um número aleatório entre 1 e 1000
+let nomeUsuario = prompt('Digite seu nome:');
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1; // Faça o computador gerar um número aleatório entre 1 e 100
 let numeroMaximoTentativas = parseInt(prompt('Digite o número máximo de tentativas:')); // Solicitar ao usuário o número máximo de tentativas
 
 // Verificar se o valor inserido é um número válido
@@ -17,7 +18,7 @@ if (isNaN(numeroMaximoTentativas) || numeroMaximoTentativas <= 0) {
   let tentativasRestantes = numeroMaximoTentativas;
 
   while (tentativasRestantes > 0) {
-    let chute = prompt(`Tente adivinhar o número entre 1 e 1000. Você ainda tem ${tentativasRestantes} tentativas.`);
+    let chute = prompt(`${nomeUsuario}, tente adivinhar o número entre 1 e 100. Você ainda tem ${tentativasRestantes} tentativas.`);
 
     // Verificar se o chute é um número válido
     if (isNaN(chute)) {
@@ -28,19 +29,18 @@ if (isNaN(numeroMaximoTentativas) || numeroMaximoTentativas <= 0) {
     chute = parseInt(chute);
 
     if (chute == numeroAleatorio) {
-      alert(`Você acertou o número em ${numeroMaximoTentativas - tentativasRestantes + 1} tentativas! Parabéns!`);
+      alert(`Parabéns, ${nomeUsuario}! Você acertou o número em ${numeroMaximoTentativas - tentativasRestantes + 1} tentativas!`);
       break;
     } else if (chute < numeroAleatorio) {
-      alert("O número secreto é maior. Está abaixo do número correto.");
+      alert(`${nomeUsuario}, o número secreto é maior. Está abaixo do número correto.`);
     } else {
-      alert("O número secreto é menor. Está acima do número correto.");
+      alert(`${nomeUsuario}, o número secreto é menor. Está acima do número correto.`);
     }
 
     tentativasRestantes--;
   }
 
   if (tentativasRestantes === 0) {
-    alert(`Você perdeu. O número correto era ${numeroAleatorio}.`);
+    alert(`Você perdeu, ${nomeUsuario}. O número correto era ${numeroAleatorio}.`);
   }
 }
-
